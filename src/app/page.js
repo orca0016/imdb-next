5d477a956893736d03d1b61262e7b62305ed2d59
mohammad <137170507+orca0016@users.noreply.github.com>
2024-09-08 21:52:38 +0430

    fetch data from tmdb api

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,23 @@
-import Image from "next/image";
+const API_KEY = process.env.API_KEY;
+import Results from '../components/Results'
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`
+  );
 
-export default function Home() {
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  const results = data.results;
+  console.log(results);
+
   return (
     <div>
-      <hr/>
-      home....
+      <Results results={results}/>
     </div>
   );
 }
