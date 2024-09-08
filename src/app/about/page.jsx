1a3faf87d931ca50262117fb039de83e030f0f9a
mohammad <137170507+orca0016@users.noreply.github.com>
2024-09-08 19:28:59 +0430

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,15 @@
+ 
+ export default function About() {
+   return (
+     <div className="max-w-6xl  mx-auto p-3 space-y-4 text-justify">
+      <h1  className="text-2xl font-medium text-amber-600">about</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, voluptate iste voluptatem soluta obcaecati fugit dolorem, ea est expedita consequuntur mollitia molestiae optio quia. Officia, veniam ducimus a excepturi dolorem eaque, accusamus aliquam obcaecati iste, maiores nulla optio est nisi perspiciatis laboriosam in expedita vero aliquid modi consectetur saepe. Modi ipsum voluptate aliquid autem ut numquam dolore dolorem dolor nulla veniam perferendis reiciendis sed magni sapiente aspernatur veritatis quasi pariatur minima, rerum itaque doloribus eveniet quaerat dicta! Quam culpa necessitatibus, quidem tenetur repellat excepturi incidunt maiores veritatis corporis reprehenderit, rerum fugit non delectus repudiandae rem nesciunt natus, nam possimus placeat.</p>
+
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum et aut voluptatibus distinctio reiciendis consequuntur, odio corrupti suscipit numquam. Exercitationem delectus quae libero animi earum aliquid, error odio quasi molestiae cupiditate rem eligendi totam quos, id accusantium molestias repellendus illo reiciendis laboriosam asperiores? Laborum molestias maxime, ullam totam reiciendis alias perspiciatis, reprehenderit laudantium, iusto similique excepturi impedit quasi. Quae, excepturi quam? Libero, dolores modi deserunt omnis quis repellat repudiandae, fugiat ab consectetur illum, esse ipsa iste minima fuga? Ratione consectetur unde, fuga aliquid quisquam, quo nostrum blanditiis rerum voluptatibus minus qui beatae eum non adipisci! Pariatur dignissimos eligendi itaque? Aut totam quos voluptatum porro dolore nulla animi rerum ullam aspernatur deserunt iste quisquam recusandae, mollitia perspiciatis in saepe nostrum, et repellendus? Numquam accusantium nobis voluptatem ipsam aliquam dolore, earum esse vitae unde eos dolorem illum incidunt libero, itaque quo accusamus reiciendis nihil voluptates dolorum corporis, deserunt dignissimos iure. Iure, harum!</p>
+
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nam illum saepe mollitia excepturi? Animi, illum accusantium sit ipsam magni ad blanditiis eveniet deserunt illo consequatur esse quidem, aliquam sapiente saepe! Sapiente, ea recusandae officia unde eius quibusdam eveniet maiores quia natus eos libero, quod saepe excepturi iusto. Optio, quia?</p>
+     </div>
+
+   )
+ }
+ 
