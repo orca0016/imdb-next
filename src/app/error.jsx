4a462e3641fb5eeff5d98afc9a9544bf597de7b2
mohammad <137170507+orca0016@users.noreply.github.com>
2024-09-09 18:01:58 +0430

    error in error.js 2

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,11 +1,10 @@
 "use client"
 
-import { useEffect } from "react"
 
-export default function error({error , reset}) {
-    useEffect(()=>{
-        console.log(error)
-    },[error])
+export default function Error({error , reset}) {
+    // useEffect(()=>{
+    //     console.log(error)
+    // },[error])
     return (
     <div className="text-center mt-10">
         <h1>Something went wrong. Please try agin later.</h1>
